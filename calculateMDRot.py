0b1efe3263c00137eca6cc4172a198cb6ce6e99f
James E T Smith <james.e.smith@colorado.edu>
2018-01-25 17:40:12 -0700

    Initial commit

---
+++ calculateMDRot.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+#the file passed to the program is the gaussian output file from which it reads
+#the geometry of the molecule
+#assumes that inertia.dat is present in the current directory and reads 
+#the inertia data
+
+import sys
+import readGeomFc
+import os
+from numpy import *
+import geomUtility
+
+#initialize parameters
+#seed = 200
+numIter = 20000
+theory = ' b3lyp/6-31G(d) '
+numprocessors = 8
+memory = ' 1000MB '
+multi = ' 2'
+
+#file = open(sys.argv[1],'r')
+inertia = open('inertia.dat','r')
+result = open('dihed_energy.out','w')
+
+outputName = '0.log'
+energy = readGeomFc.readHFEnergy(outputName)
+outFile = open('0.log','r')
+(geom,Mass) = readGeomFc.readGeom(outFile)
+
+(rotors) = readGeomFc.readGeneralInertia(inertia,Mass)
+numRotors = len(rotors)-1
+
+
+
+atoms = readGeomFc.getAtoms(Mass)
+diheds=numRotors*[0]
+for i in range(numRotors):
+    result.write('%10.3f'%diheds[i])
+result.write('%14.7f'%energy+'\n')
+
+
+for i in range(12**(numRotors)):
+
+#    diheds = random.rand(len(rotors)-1)*360
+    irem = i
+    for j in range(len(rotors)-1):
+        diheds[j] = (irem-irem/12*12)*30.0
+        irem = irem/12
+        result.write('%10.3f'%diheds[j])
+    result.write('\n')
+
+    newGeom = geomUtility.rotateDihedrals(geom,rotors,diheds,Mass)
+
+    file = open(str(i+1)+'.com','w')
+    file.write('%mem='+memory+'\n')
+    file.write('%nproc='+ str(numprocessors)+'\n')
+    file.write('#'+theory+' nosym\n\nTitle Card Required\n\n0'+multi+'\n')
+    for j in (range(len(Mass))):
+        file.write(atoms[j] + '  '+str(float(newGeom[j,0]))+ '  '+str(float(newGeom[j,1]))+ '  '+str(float(newGeom[j,2]))+'\n')
+    file.write('\n\n')
+    file.close()
+
+    #os.system("g03 "+str(i+1)+".com")
+    #outputName = str(i+1)+'.log'
+    #energy = readGeomFc.readEnergy(outputName)
+    #result.write('%14.7f'%energy+'\n')
+
+result.close()
+
+
---
+++ fitFourier.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import os
+import sys
+from Harmonics import *
+
+file = sys.argv[1]
+
+
+Kcos=[]
+Ksin =[]
+harmonic = Harmonics(5,Kcos,Ksin)
+harmonic.fitPotential(file)
+
+for i in range(5):
+    print harmonic.Kcos[i], harmonic.Ksin[i]
+
+for i in range(5):
+    print harmonic.Kcos[i], " ", "%3.1f"%(float(i+1))
+for i in range(5):
+    print harmonic.Ksin[i], " ", "%3.1f"%(float(i+1))
+
+
+
+
