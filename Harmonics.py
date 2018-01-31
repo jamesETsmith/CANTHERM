d94f215f4527729891ad8a16cfdf3f90d3e503b0
jamesETsmith <jasm3285@colorado.edu>
2018-01-31 10:53:45 -0700

    Small changes to allow for compatibility with Python 2.7 or 3.6.

--- Harmonics.py
+++ Harmonics.py
@@ -2,81 +2,85 @@ from numpy import *
 import pdb
 import readGeomFc
 import math
-import Gnuplot,Gnuplot.funcutils
+#import Gnuplot
+#import Gnuplot.funcutils
+
 
 class Harmonics:
-    numFit = 0
-    Kcos = []
-    Ksin = []
-    A = 0.0
+  numFit = 0
+  Kcos = []
+  Ksin = []
+  A = 0.0
 
-    def __init__(self,numFit, Kcos, Ksin):
-        self.numFit = numFit
-        self.Kcos = Kcos
-        self.Ksin = Ksin
+  def __init__(self, numFit, Kcos, Ksin):
+    self.numFit = numFit
+    self.Kcos = Kcos
+    self.Ksin = Ksin
 
-    def getPotential(self, angle):
-#assuming the angle is in degrees
-        rad = angle*math.pi/180
-        pot = self.A
-        for i in range(self.numFit):
-           pot = pot + self.Ksin[i]*math.sin((i+1)*rad) + self.Kcos[i]*math.cos((i+1)*rad)
-        return pot
+  def getPotential(self, angle):
+    # assuming the angle is in degrees
+    rad = angle * math.pi / 180
+    pot = self.A
+    for i in range(self.numFit):
+      pot = pot + \
+          self.Ksin[i] * math.sin((i + 1) * rad) + \
+          self.Kcos[i] * math.cos((i + 1) * rad)
+    return pot
 
-    def fitPotential(self,file):
-        read = open(file,'r')
-        lines = read.readlines()
-        angles = []
-        potentials = []
-        pot0 = 0.0
-        nfit = len(lines)-4 #first three lines are comments and last time is repeat
-        potgiven = []
-        for i in range(3,len(lines)-1):
-           tokens = lines[i].split()
+  def fitPotential(self, file):
+    read = open(file, 'r')
+    lines = read.readlines()
+    angles = []
+    potentials = []
+    pot0 = 0.0
+    # first three lines are comments and last time is repeat
+    nfit = len(lines) - 4
+    potgiven = []
+    for i in range(3, len(lines) - 1):
+      tokens = lines[i].split()
 
-           potentials.append(float(tokens[1]))
-           if i == 3:
-              pot0 = potentials[0]
-           potentials[i-3] = (potentials[i-3]-pot0)*627.5095
-           potgiven.append([float(i-3)*360/nfit, potentials[i-3]])
+      potentials.append(float(tokens[1]))
+      if i == 3:
+        pot0 = potentials[0]
+      potentials[i - 3] = (potentials[i - 3] - pot0) * 627.5095
+      potgiven.append([float(i - 3) * 360 / nfit, potentials[i - 3]])
 
-        #now fit the potentials
-        Y = transpose(matrix(potentials[:nfit]))
-        X = matrix(zeros((nfit,11),dtype=float))
-        for i in range(nfit):
-           angle = float(i)*2*math.pi/nfit
-           X[i,0]=1.0
-           for j in range(5):
-              X[i,j+1] = math.cos((j+1)*angle)
-              X[i,6+j] = math.sin((j+1)*angle)
+    # now fit the potentials
+    Y = transpose(matrix(potentials[:nfit]))
+    X = matrix(zeros((nfit, 11), dtype=float))
+    for i in range(nfit):
+      angle = float(i) * 2 * math.pi / nfit
+      X[i, 0] = 1.0
+      for j in range(5):
+        X[i, j + 1] = math.cos((j + 1) * angle)
+        X[i, 6 + j] = math.sin((j + 1) * angle)
 
-        XtX = transpose(X)*X
-        XtY = transpose(X)*Y
-        b = linalg.inv(XtX)*XtY
-        
-        for i in range(5):
-           self.Kcos.append(0.0)
-           self.Kcos[i] = float(b[i+1])
-           self.Ksin.append(0.0)
-           self.Ksin[i] = float(b[i+6])
-        self.A = float(b[0])
-        self.numFit = 5
+    XtX = transpose(X) * X
+    XtY = transpose(X) * Y
+    b = linalg.inv(XtX) * XtY
 
-        #print self.Kcos
-        #print self.Ksin
+    for i in range(5):
+      self.Kcos.append(0.0)
+      self.Kcos[i] = float(b[i + 1])
+      self.Ksin.append(0.0)
+      self.Ksin[i] = float(b[i + 6])
+    self.A = float(b[0])
+    self.numFit = 5
 
-        #print "Potential-Read Potential-Fit"
-        pot = []
-        for i in range(3*nfit+1):
-           angle = i*360/3/nfit
-           pot.append([angle,self.getPotential(angle)])
-        #   print '%14.2f'%potentials[i]+'%14.3f'%pot[i]
-        #print
-        #g=Gnuplot.Gnuplot()
-        #g('set data style linespoints')
-        #g('set xrange [0:360]')
-        #g.plot(potgiven,pot)
-        #raw_input('Please press enter to continue ...\n')
- 
-        
-        return
+    # print self.Kcos
+    # print self.Ksin
+
+    # print "Potential-Read Potential-Fit"
+    pot = []
+    for i in range(3 * nfit + 1):
+      angle = i * 360 / 3 / nfit
+      pot.append([angle, self.getPotential(angle)])
+    #   print '%14.2f'%potentials[i]+'%14.3f'%pot[i]
+    # print
+    # g=Gnuplot.Gnuplot()
+    #g('set data style linespoints')
+    #g('set xrange [0:360]')
+    # g.plot(potgiven,pot)
+    #raw_input('Please press enter to continue ...\n')
+
+    return
--- rotateGeom.f90
+++ rotateGeom.f90
@@ -1,27 +1,27 @@
-subroutine rotateGeom(numAtoms, Geom,numRotors, rotors, dihedrals, Mass, Geomout)
+SUBROUTINE rotateGeom(numAtoms, Geom,numRotors, rotors, dihedrals, Mass, Geomout)
 
   !rotors contains other pivot atom, this pivot atom and num atoms in rotor and list of atoms
 
-  implicit none
-  double precision Geom(100,3), diheds(20), Mass(100), Geomout(100,3), pivotcoord(3)
-  integer rotors(100,20), numRotors, i, numAtoms, j, k
+  IMPLICIT NONE
+  DOUBLE PRECISION Geom(100,3), diheds(20), Mass(100), Geomout(100,3), pivotcoord(3)
+  INTEGER rotors(100,20), numRotors, i, numAtoms, j, k
 
- !copy the geometry
-  do i=1,numAtoms
-     do j=1,3
+  !copy the geometry
+  DO i=1,numAtoms
+     DO j=1,3
         Geomout(i,j) = Geom(i,j)
-     end do
-  end do
+     END DO
+  END DO
 
- !move the 
-  do i=1,numRotors
+  !move the
+  DO i=1,numRotors
 
-     do j=1,3
+     DO j=1,3
         pivotcoord(j) = Geom(rotors(i,2),j)
-     end do
-     
-     do j=1,numAtoms
-        do k=1,3
+     END DO
+
+     DO j=1,numAtoms
+        DO k=1,3
            Geomout(j,k) = Geomout(j,k) - pivotcoord(k)
-        end do
-     end do
+        END DO
+     END DO
