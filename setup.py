4b306e61a968e4416f04410e547084058fb39456
jamesETsmith <jasm3285@colorado.edu>
2019-08-14 21:18:01 -0600

    Adding simple setup.py and installation instructions.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='CANTHERM',
+    version='0.1.0',
+    author='James E T Smith',
+    author_email='james.smith9113@gmail.com',
+    packages=['cantherm'],
+    license='LICENSE',
+    description=
+    'Thermochemistry post processing for quantum chemistry packages',
+    long_description=open('README.md').read(),
+)
